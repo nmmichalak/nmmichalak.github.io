dfd5219fcf821a6074c77d2849a20808f8d5d71f
Nicholas Michalak <michalak.nicholas@gmail.com>
2020-10-29 13:24:51 -0600

    update experiment example script
    2020C experiment workshop

--- 2020insight_experiment_workshop_examples.py
+++ 2020insight_experiment_workshop_examples.py
@@ -3,12 +3,16 @@ from math import exp
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
 import linearmodels as lm
 
 # set pyplot style
 plt.style.use(["ggplot", "seaborn-colorblind"])
+
+# seaborn style
+sns.set(palette = "colorblind", font_scale = 1.5)
 
 # example experiment data
 experiment = pd.read_csv("ivexample.csv")
@@ -95,5 +99,51 @@ ax.scatter(x = d, y = n_80power)
 ax.plot(d, n_80power)
 
 # titles
-ax.set_xlabel("Effect size in stadard deviation units")
+ax.set_xlabel("Effect size in standard deviation units")
 ax.set_title("Number of users needed to detect a given effect size\ntwo independent groups, 80% power, $\\alpha = 0.05$, two-sided alternative")
+
+# packages
+import numpy as np
+import pandas as pd
+import statsmodels.formula.api as smf
+
+# difference-in-difefrence example
+## parameters
+N = 100
+b0 = 20
+b1 = -1.5
+b2 = -1
+b3 = 3
+
+## data
+np.random.seed(357904)
+wage_increase = np.repeat([-0.5, 0.5], repeats = N / 2)
+period_change = np.repeat([-0.5, 0.5, -0.5, 0.5], repeats = N / 4)
+full_time = b0 + b1 * (wage_increase) + b2 * (period_change) + b3 * (wage_increase * period_change) + np.random.normal(loc = 0, scale = 1, size = N)
+did_data = pd.DataFrame({"full_time": full_time, "period_change": period_change, "wage_increase": wage_increase})
+
+# labels
+did_data["state"] = did_data["wage_increase"].replace({-0.5: "Pennsylvania", 0.5: "New Jersey"}).astype("category").cat.reorder_categories(["Pennsylvania", "New Jersey"])
+did_data["period"] = did_data["period_change"].replace({-0.5: "Before", 0.5: "After"}).astype("category").cat.reorder_categories(["Before", "After"])
+
+# plot
+plt.figure(figsize = (12, 6))
+sns.violinplot(x = "period", y = "full_time", hue = "state", data = did_data)
+plt.legend(loc = "best")
+plt.xlabel(None)
+plt.ylabel(None)
+plt.title("Full-Time Equivalent")
+plt.tight_layout()
+
+# regression
+## create interaction term
+did_data["did"] = np.where((period_change == -0.5) & (wage_increase == -0.5), 0.5, 
+np.where((period_change == -0.5) & (wage_increase == 0.5), -0.5, 
+np.where((period_change == 0.5) & (wage_increase == -0.5), -0.5, 
+np.where((period_change == 0.5) & (wage_increase == 0.5), 0.5, np.nan))))
+
+# fit regression
+ols_fit3 = smf.ols("full_time ~ period_change + wage_increase + did", data = did_data).fit()
+
+# results
+ols_fit3.summary()
